70a490464e37557d578757478e5dc73ae185b4e2
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-03-12 21:01:43 +0300

    initial

---
+++ GerundOrInfinitive.Presentation/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GerundOrInfinitive.Presentation;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ GerundOrInfinitive.Presentation/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GerundOrInfinitive.Presentation;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
