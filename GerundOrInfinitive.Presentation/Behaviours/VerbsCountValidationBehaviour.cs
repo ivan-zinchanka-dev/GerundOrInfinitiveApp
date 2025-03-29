a34fa15b0f762a5822c192a9c395e891843104a2
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-03-29 22:30:11 +0300

    feat: validation, p.1

---
+++ GerundOrInfinitive.Presentation/Behaviours/VerbsCountValidationBehaviour.cs
@@ -0,0 +1,42 @@
+﻿namespace GerundOrInfinitive.Presentation.Behaviours;
+
+public class VerbsCountValidationBehaviour : Behavior<Entry>
+{
+    public static readonly BindableProperty ErrorMessageProperty = BindableProperty.Create(
+        nameof(ErrorMessage), 
+        typeof(string), 
+        typeof(VerbsCountValidationBehaviour), 
+        string.Empty, 
+        BindingMode.TwoWay);
+
+    public string ErrorMessage
+    {
+        get => (string)GetValue(ErrorMessageProperty);
+        set => SetValue(ErrorMessageProperty, value);
+    }
+    
+    
+    protected override void OnAttachedTo(Entry entry)
+    {
+        entry.TextChanged += OnTextChanged;
+        base.OnAttachedTo(entry);
+    }
+
+    protected override void OnDetachingFrom(Entry entry)
+    {
+        entry.TextChanged -= OnTextChanged;
+        base.OnDetachingFrom(entry);
+    }
+
+    private void OnTextChanged(object sender, TextChangedEventArgs eventArgs)
+    {
+        if (int.TryParse(eventArgs.NewTextValue, out int verbsCount))
+        {
+            
+        }
+        else
+        {
+            ErrorMessage = "Please enter a number";
+        }
+    }
+}
