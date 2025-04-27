59c033c75a7a3273ed9d3b499aef89f231d737ca
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-04-27 23:44:54 +0300

    feat: multiplatform stepper, p.2

--- GerundOrInfinitive.Presentation/Views/Controls/Stepper.xaml.cs
+++ GerundOrInfinitive.Presentation/Views/Controls/Stepper.xaml.cs
@@ -2,31 +2,36 @@
 
 public partial class Stepper : ContentView
 {
-	private int _digits = 4;
+	private static class RoundingDigits
+	{
+		public const int Min = 1;
+		public const int Default = 4;
+		public const int Max = 15;
+	}
 	
-	public static readonly BindableProperty MaximumProperty = BindableProperty.Create(nameof(Maximum), typeof(double), typeof(Stepper), 100.0,
-		validateValue: (bindable, value) => (double)value > ((Stepper)bindable).Minimum,
+	private static class Defaults
+	{
+		public const double Max = 100.0;
+		public const double Min = 0.0;
+		public const double Increment = 1.0;
+	}
+	
+	private int _roundingDigits = RoundingDigits.Default;
+	
+	public static readonly BindableProperty MaximumProperty = BindableProperty.Create(
+		nameof(Maximum), typeof(double), typeof(Stepper), Defaults.Max,
+		validateValue: (bindable, value) => (double)value > ((Stepper)bindable).Minimum);
+	
+	public static readonly BindableProperty MinimumProperty = BindableProperty.Create(
+		nameof(Minimum), typeof(double), typeof(Stepper), Defaults.Min,
+		validateValue: (bindable, value) => (double)value < ((Stepper)bindable).Maximum);
+	
+	public static readonly BindableProperty ValueProperty = BindableProperty.Create(
+		nameof(Value), typeof(double), typeof(Stepper), Defaults.Min, BindingMode.TwoWay,
 		coerceValue: (bindable, value) =>
 		{
 			var stepper = (Stepper)bindable;
-			stepper.Value = Math.Clamp((double)value, stepper.Minimum, stepper.Maximum);
-			return value;
-		});
-	
-	public static readonly BindableProperty MinimumProperty = BindableProperty.Create(nameof(Minimum), typeof(double), typeof(Stepper), 0.0,
-		validateValue: (bindable, value) => (double)value < ((Stepper)bindable).Maximum,
-		coerceValue: (bindable, value) =>
-		{
-			var stepper = (Stepper)bindable;
-			stepper.Value = Math.Clamp((double)value, stepper.Minimum, stepper.Maximum);
-			return value;
-		});
-	
-	public static readonly BindableProperty ValueProperty = BindableProperty.Create(nameof(Value), typeof(double), typeof(Stepper), 0.0, BindingMode.TwoWay,
-		coerceValue: (bindable, value) =>
-		{
-			var stepper = (Stepper)bindable;
-			double roundValue = Math.Round((double)value, stepper._digits);
+			double roundValue = Math.Round((double)value, stepper._roundingDigits);
 			return Math.Clamp(roundValue, stepper.Minimum, stepper.Maximum);
 		},
 		propertyChanged: (bindable, oldValue, newValue) =>
@@ -35,15 +40,10 @@ public partial class Stepper : ContentView
 			stepper.ValueChanged?.Invoke(stepper, new ValueChangedEventArgs((double)oldValue, (double)newValue));
 		});
 	
-	public static readonly BindableProperty IncrementProperty = BindableProperty.Create(nameof(Increment), typeof(double), typeof(Stepper), 1.0,
+	public static readonly BindableProperty IncrementProperty = BindableProperty.Create(
+		nameof(Increment), typeof(double), typeof(Stepper), Defaults.Increment,
 		propertyChanged: OnIncrementPropertyChanged);
 	
-	public double Increment
-	{
-		get => (double)GetValue(IncrementProperty);
-		set => SetValue(IncrementProperty, value);
-	}
-
 	public double Maximum
 	{
 		get => (double)GetValue(MaximumProperty);
@@ -61,41 +61,54 @@ public partial class Stepper : ContentView
 		get => (double)GetValue(ValueProperty);
 		set => SetValue(ValueProperty, value);
 	}
+	
+	public double Increment
+	{
+		get => (double)GetValue(IncrementProperty);
+		set => SetValue(IncrementProperty, value);
+	}
 
 	public event EventHandler<ValueChangedEventArgs> ValueChanged;
 	
 	public Stepper()
     {
         InitializeComponent();
+        
+        ValueChanged += UpdateButtonStates;
+        Unloaded += OnUnloaded;
     }
-    
-	public Stepper(double min, double max, double value, double increment) : this()
-	{
-		if (min >= max)
-			throw new ArgumentOutOfRangeException(nameof(min));
-		if (max > Minimum)
-		{
-			Maximum = max;
-			Minimum = min;
-		}
-		else
-		{
-			Minimum = min;
-			Maximum = max;
-		}
 
-		Increment = increment;
-		Value = Math.Clamp(value, min, max);
+	private void UpdateButtonStates(double value)
+	{
+		_minusButton.IsEnabled = value > Minimum;
+		_plusButton.IsEnabled = value < Maximum;
+	}
+	
+	private void UpdateButtonStates(object sender, ValueChangedEventArgs eventArgs)
+	{
+		UpdateButtonStates(eventArgs.NewValue);
 	}
 	
 	private static void OnIncrementPropertyChanged(BindableObject bindable, object oldValue, object newValue)
 	{
-		const int minDigits = 1;
-		const int digitsAddend = 4;
-		const int maxDigits = 15;
-		
-		((Stepper)bindable)._digits = 
-			Math.Clamp((int)(-Math.Log10((double)newValue) + digitsAddend), minDigits, maxDigits);
+		((Stepper)bindable)._roundingDigits = 
+			Math.Clamp((int)(-Math.Log10((double)newValue) + RoundingDigits.Default), 
+				RoundingDigits.Min, RoundingDigits.Max);
+	}
+
+	private void OnMinusButtonClick(object sender, EventArgs eventArgs)
+	{
+		Value -= Increment;
 	}
 	
+	private void OnPlusButtonClick(object sender, EventArgs eventArgs)
+	{
+		Value += Increment;
+	}
+	
+	private void OnUnloaded(object sender, EventArgs eventArgs)
+	{
+		ValueChanged -= UpdateButtonStates;
+		Unloaded -= OnUnloaded;
+	}
 }
